df4157572a06af1bab4428641d010623fd5cad21
BruceLi <liyifei723@gmail.com>
2020-12-30 15:31:44 +0800

    Update lab5.1 Item

---
+++ src/labs/lab5_2/Item.java
@@ -0,0 +1,33 @@
+package labs.lab5_2;
+
+public class Item {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Item(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
