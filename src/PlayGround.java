bc4cfe9d28a0a50448cc7ee54c17e06f0c1a3538
BruceLi <liyifei723@gmail.com>
2021-03-03 15:55:26 +0800

    Update ppt6

--- src/PlayGround.java
+++ src/PlayGround.java
@@ -1,9 +1,13 @@
 import java.util.*;
 
 public class PlayGround {
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double n = input.nextInt();
-        System.out.println(n);
+        final int[] s1 = new int[3];
+        int[] s2 = {0,0,0};
+        //s1 = s2;
+        s1[2] = 9;
+        System.out.println(s1[2]);
+
     }
 }
