7d4186cedc780cc3d4d4f01cfa0c3cf2249689b2
BruceLi <liyifei723@gmail.com>
2021-03-08 13:20:36 +0800

    Update ppt6

---
+++ src/examples/ppt6/CoinFlip.java
@@ -0,0 +1,30 @@
+package examples.ppt6;
+
+public class CoinFlip {
+    public static void main (String[] args){
+        examples.ppt6.Coin myCoin = new Coin(); // create the object of the Coin class
+
+        myCoin.setKey(9988);
+        myCoin.lock(9988);
+
+        myCoin.flip(); // flip the coin to see the result
+        System.out.println(myCoin); // print the results according to toString method
+        if (myCoin.isHeads()){
+            System.out.println("You WIN, congratulations!");
+        }
+        else {
+            System.out.println("Sorry, better luck next time!");
+        }
+
+        myCoin.unlock(9988);
+
+        myCoin.flip(); // flip the coin to see the result
+        System.out.println(myCoin); // print the results according to toString method
+        if (myCoin.isHeads()){
+            System.out.println("You WIN, congratulations!");
+        }
+        else {
+            System.out.println("Sorry, better luck next time!");
+        }
+    }
+}
