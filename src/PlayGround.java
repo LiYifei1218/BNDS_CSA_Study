3e38080c12b0f8d1495e4d5a0f5f3dc9401fb352
BruceLi <liyifei723@gmail.com>
2021-03-08 12:51:24 +0800

    Update ppt6

--- src/PlayGround.java
+++ src/PlayGround.java
@@ -3,11 +3,7 @@ import java.util.*;
 public class PlayGround {
 
     public static void main(String[] args) {
-        final int[] s1 = new int[3];
-        int[] s2 = {0,0,0};
-        //s1 = s2;
-        s1[2] = 9;
-        System.out.println(s1[2]);
+        System.out.println((flo)6/(2+3));
 
     }
 }
